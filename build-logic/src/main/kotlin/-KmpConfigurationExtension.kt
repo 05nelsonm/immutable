5614f5fc2c7c1361901130cbb1464c7361d6434f
Matthew Nelson <developer@matthewnelson.io>
2024-08-30 04:54:11 -0400

    Use unique moduleName parameters

--- build-logic/src/main/kotlin/-KmpConfigurationExtension.kt
+++ build-logic/src/main/kotlin/-KmpConfigurationExtension.kt
@@ -24,6 +24,10 @@ fun KmpConfigurationExtension.configureShared(
     action: Action<KmpConfigurationContainerDsl>
 ) {
     configure {
+        options {
+            useUniqueModuleNames = true
+        }
+
         jvm {
             kotlinJvmTarget = JavaVersion.VERSION_1_8
             compileSourceCompatibility = JavaVersion.VERSION_1_8
